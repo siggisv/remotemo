e787d608e48ed37d7ba56cb3b52dda178433f876
Sigurður Sveinn Halldórsson <siggisv@gmail.com>
2022-11-29 22:22:00 +0100

    Make linters happy (#3)
    ... and config them to make me happy.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ really old-school looking user interface.
 
 [![GitHub Super-Linter](https://github.com/siggisv/remotemo/workflows/Lint%20Code%20Base/badge.svg)](https://github.com/marketplace/actions/super-linter)
 
-By really old-school, we mean late 70s - beginning of the 80s. A monochrome, fixed size, 
+By really old-school, we mean late 70s - beginning of the 80s. A monochrome, fixed size,
 text-only monitor with keyboard-only user input (and restricted to ASCII only, not even extended ASCII).
 
 Optionally the text gets displayed at a slow rate to recreate the feeling of being connected over a slow connection.
--- src/remotemo.cpp
+++ src/remotemo.cpp
@@ -5,12 +5,12 @@
 const int WINDOW_WIDTH = 1280;
 const int WINDOW_HEIGHT = 720;
 
-const char* screenImageFile = "terminal_screen.png";
+const char *screenImageFile = "terminal_screen.png";
 const SDL_Point SCREEN_TEXTURE_SIZE = {946, 732};
-const SDL_Rect backgroundMinArea {.x=118, .y=95, .w=700, .h=540};
+const SDL_Rect backgroundMinArea {.x = 118, .y = 95, .w = 700, .h = 540};
 
 
-SDL_Texture* loadTexture(SDL_Renderer *renderer, const std::string &file)
+SDL_Texture *loadTexture(SDL_Renderer *renderer, const std::string &file)
 {
     auto texture = IMG_LoadTexture(renderer, file.c_str());
     if (texture == nullptr) {
@@ -21,21 +21,20 @@ SDL_Texture* loadTexture(SDL_Renderer *renderer, const std::string &file)
     return texture;
 }
 
-void getClipping(const int win_w, const int win_h,
-        const int texture_w, const int texture_h,
-        const SDL_Rect &minArea,
-        float &scale, SDL_Rect &target)
+void getClipping(const int win_w, const int win_h, const int texture_w,
+        const int texture_h, const SDL_Rect &minArea, float *scale,
+        SDL_Rect *target)
 {
-    target.h = texture_h;
-    target.w = texture_w;
-    float scale_w = (float)win_w / minArea.w;
-    float scale_h = (float)win_h / minArea.h;
-    scale = scale_w < scale_h ? scale_w : scale_h;
+    target->h = texture_h;
+    target->w = texture_w;
+    float scale_w = static_cast<float>(win_w) / minArea.w;
+    float scale_h = static_cast<float>(win_h) / minArea.h;
+    *scale = scale_w < scale_h ? scale_w : scale_h;
 
-    int border_h = ((win_h / scale) - minArea.h + 1) / 2;
-    int border_w = ((win_w / scale) - minArea.w + 1) / 2;
-    target.y = border_h - minArea.y;
-    target.x = border_w - minArea.x;
+    int border_h = ((win_h / *scale) - minArea.h + 1) / 2;
+    int border_w = ((win_w / *scale) - minArea.w + 1) / 2;
+    target->y = border_h - minArea.y;
+    target->x = border_w - minArea.x;
 }
 
 int test_init()
@@ -52,29 +51,24 @@ int test_init()
                 SDL_GetError());
     }
 
-    auto window = SDL_CreateWindow(
-            "Monitor TEST",
-            SDL_WINDOWPOS_UNDEFINED,
-            SDL_WINDOWPOS_UNDEFINED,
-            WINDOW_WIDTH,
-            WINDOW_HEIGHT,
-            SDL_WINDOW_RESIZABLE
-    );
+    auto window = SDL_CreateWindow("Monitor TEST", SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT,
+            SDL_WINDOW_RESIZABLE);
     if (window == nullptr) {
         SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION,
                 "SDL_CreateWindow() failed: %s\n", SDL_GetError());
         SDL_Quit();
-        return 2 ;
+        return 2;
     }
 
-    auto renderer = SDL_CreateRenderer(window, -1,
-            SDL_RENDERER_TARGETTEXTURE);
+    auto renderer =
+            SDL_CreateRenderer(window, -1, SDL_RENDERER_TARGETTEXTURE);
     if (renderer == NULL) {
         SDL_DestroyWindow(window);
         SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION,
                 "SDL_CreateRenderer() failed: %s\n", SDL_GetError());
         SDL_Quit();
-        return 2 ;
+        return 2;
     }
 
     char *basePath = SDL_GetBasePath();
@@ -98,19 +92,19 @@ int test_init()
     resImagePath = resImagePath + "res" + PATH_SEP + "img" + PATH_SEP;
     SDL_Log("resImagePath: %s\n", resImagePath.c_str());
 
-    auto screenTexture = loadTexture(renderer,
-            resImagePath + screenImageFile);
+    auto screenTexture =
+            loadTexture(renderer, resImagePath + screenImageFile);
     if (screenTexture == nullptr) {
         SDL_DestroyRenderer(renderer);
         SDL_DestroyWindow(window);
         SDL_Quit();
-        return 2 ;
+        return 2;
     }
     SDL_Rect backgroundTarget;
     float backgroundScale;
-    getClipping(WINDOW_WIDTH, WINDOW_HEIGHT,
-            SCREEN_TEXTURE_SIZE.x, SCREEN_TEXTURE_SIZE.y,
-            backgroundMinArea, backgroundScale, backgroundTarget);
+    getClipping(WINDOW_WIDTH, WINDOW_HEIGHT, SCREEN_TEXTURE_SIZE.x,
+            SCREEN_TEXTURE_SIZE.y, backgroundMinArea, &backgroundScale,
+            &backgroundTarget);
 
     bool done = false;
     bool fullscreen = false;
@@ -118,7 +112,7 @@ int test_init()
     bool keyHandled_F11 = false;
     while (!done && SDL_WaitEvent(&event)) {
         bool unhandled_event = true;
-        while(!done && unhandled_event) {
+        while (!done && unhandled_event) {
             switch (event.type) {
                 case SDL_QUIT:
                     done = true;
@@ -127,22 +121,22 @@ int test_init()
                     switch (event.key.keysym.sym) {
                         case SDLK_q:
                         case SDLK_w:
-                            if(SDL_GetModState() & KMOD_CTRL) {
+                            if (SDL_GetModState() & KMOD_CTRL) {
                                 done = true;
                             }
                             break;
                         case SDLK_F11:
-                            if (keyHandled_F11) break;
+                            if (keyHandled_F11)
+                                break;
                             /* Do the following only once per keypress.
                              * Therefore we set keyHandled_F11 when done
                              * and then reset it back to false when KEYUP.
-                            */
+                             */
                             if (!fullscreen) {
                                 SDL_SetWindowFullscreen(window,
                                         SDL_WINDOW_FULLSCREEN_DESKTOP);
                             } else {
-                                SDL_SetWindowFullscreen(window,
-                                        0);
+                                SDL_SetWindowFullscreen(window, 0);
                             }
                             fullscreen = !fullscreen;
                             keyHandled_F11 = true;
@@ -163,11 +157,9 @@ int test_init()
                     switch (event.window.event) {
                         case SDL_WINDOWEVENT_SIZE_CHANGED:
                             getClipping(event.window.data1,
-                                    event.window.data2,
-                                    SCREEN_TEXTURE_SIZE.x,
-                                    SCREEN_TEXTURE_SIZE.y,
-                                    backgroundMinArea,
-                                    backgroundScale, backgroundTarget);
+                                    event.window.data2, SCREEN_TEXTURE_SIZE.x,
+                                    SCREEN_TEXTURE_SIZE.y, backgroundMinArea,
+                                    &backgroundScale, &backgroundTarget);
                             break;
                         default:
                             break;
