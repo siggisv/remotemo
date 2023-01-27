e79d123d44434909d3cfd0f363230d5b8292b7c3
Sigurður Sveinn Halldórsson <siggisv@gmail.com>
2023-01-27 20:22:29 +0100

    Add implementation that passes those tests

--- src/remotemo.cpp
+++ src/remotemo.cpp
@@ -3,12 +3,25 @@
 
 namespace remoTemo {
 Temo::~Temo() noexcept
-{}
+{
+  if (m_cleanup_all) {
+    SDL_Quit();
+  } else {
+    SDL_QuitSubSystem(sdl_init_flags);
+  }
+}
 Temo::Temo(Temo&& other) noexcept
     : m_cleanup_all(std::exchange(other.m_cleanup_all, false))
 {}
-bool Temo::initialize([[maybe_unused]] const Config& config)
+bool Temo::initialize(const Config& config)
 {
+  if (SDL_Init(sdl_init_flags) < 0) {
+    SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION, "SDL_Init() failed: %s\n",
+        SDL_GetError());
+    return false;
+  } else {
+    m_cleanup_all = config.m_cleanup_all;
+  }
   return true;
 }
 std::optional<Temo> create()
