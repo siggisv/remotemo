5546c34caa984816e5bd14a238d1acdb5ff115fd
Sigurður Sveinn Halldórsson <siggisv@gmail.com>
2023-10-01 22:56:02 +0200

    Change conanfile to also override sdl version

--- conanfile.py
+++ conanfile.py
@@ -5,6 +5,6 @@ class Remotemo(ConanFile):
     generators = "CMakeToolchain", "CMakeDeps"
 
     def requirements(self):
-        self.requires("sdl/[~2.26]")
+        self.requires("sdl/[~2]", override=True)
         self.requires("sdl_image/[~2]")
         self.requires("libwebp/[~1.3]", override=True)
